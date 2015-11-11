73b9014a50dbcb4fb3be62596089d43d2a2e8349
Fran Arroyo <francisco.arroyoeliche@gmail.com>
2015-11-11 14:56:52 +0100

    modificado: ../Interes.java

--- Interes.java
+++ Interes.java
@@ -3,6 +3,11 @@
 //el cúal  es directamente proporcional al capital inicial (C),
 // al tiempo (t), y a la tasa de interés (i) :
 //I = C · i · t
+/*
+ * @author Fran
+ * @version 1.0
+ */
+
 
 public class Interes{
 	public static double calcularInter(double c, double i, double t){
