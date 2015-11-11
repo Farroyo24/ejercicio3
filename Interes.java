2b157f9b5744ef6bc84122e671bb75755f64dd4e
Fran Arroyo <francisco.arroyoeliche@gmail.com>
2015-11-11 15:08:38 +0100

    modificado: Interes.java

--- Interes.java
+++ Interes.java
@@ -1,11 +1,11 @@
-/**fichero Interes.java
- *Clase el interés (I) que produce un capital
- *el cúal  es directamente proporcional al capital inicial (C),
- *al tiempo (t), y a la tasa de interés (i) :
- *I = C · i · t
- *
- * @author: Fran
- * @version: 1.0
+//fichero Interes.java
+//Clase el interés (I) que produce un capital
+//el cúal  es directamente proporcional al capital inicial (C),
+//al tiempo (t), y a la tasa de interés (i) :
+//I = C · i · t
+/**
+ * @author Fran
+ * @version 1.0
  */
 
 
